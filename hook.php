f39ac1cc4c64820acd984d7581dc9667b0944953
ANTVirGEO <anton.bereza@academmedia.com>
2016-12-09 16:36:33 +0700

    added hook

--- hook.php
+++ hook.php
@@ -4,16 +4,14 @@ require __DIR__ . '/vendor/autoload.php';
 
 $API_KEY = '301591460:AAEhJBSP6m7Ym-O8TQN5-zDWvhYIsPZTAiU';
 $BOT_NAME = 'arkadii_bot';
-$hook_url = 'https://arkadii.herokuapp.com/hook.php';
 try {
     // Create Telegram API object
     $telegram = new Longman\TelegramBot\Telegram($API_KEY, $BOT_NAME);
 
-    // Set webhook
-    $result = $telegram->setWebHook($hook_url);
-    if ($result->isOk()) {
-        echo $result->getDescription();
-    }
+    // Handle telegram webhook request
+    $telegram->handle();
 } catch (Longman\TelegramBot\Exception\TelegramException $e) {
-    echo $e;
+    // Silence is golden!
+    // log telegram errors
+    // echo $e;
 }
