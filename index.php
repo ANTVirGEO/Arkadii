a8eb1e2ea84aedef7fc3f21f34f41962764a0aff
ANTVirGEO <anton.bereza@academmedia.com>
2016-12-10 16:22:47 +0700

    test

--- index.php
+++ index.php
@@ -1,29 +1,20 @@
 <?php
-
 set_time_limit(0);
-
 // Установка токена
-
 $botToken = "301591460:AAEhJBSP6m7Ym-O8TQN5-zDWvhYIsPZTAiU";
 $website = "https://api.telegram.org/bot" . $botToken;
-
 // Получаем запрос от Telegram
-
 $content = file_get_contents("php://input");
 $update = json_decode($content, TRUE);
 $message = $update["message"];
-
 // Получаем внутренний номер чата Telegram и команду, введённую пользователем в   чате
-
 $chatId = $message["chat"]["id"];
 $text = $message["text"];
-
 // Пример обработки команды /start
-
-if ($text == '/start') {
-    $welcomemessage = 'АРКААААААААААААДИЙ!!!';
-
-    // Отправляем сформированное сообщение обратно в Telegram пользователю
-
-    file_get_contents($website . "/sendmessage?chat_id=" . $chatId . "&text=" . $welcomemessage);
-}
+if ($text == '/start')
+    $output = 'АРКААААААААААААДИЙ!!!';
+elseif ($text == 'тест')
+    $output = "ТЕСТ-ТЕСТ-ТЕСТ!!!";
+else
+    $output = "WTF r u talking about! ЕВГЕЕЕЕЕНИИИИИИИЙ ИВААААААНООООВИИИИИЧ!!!!";
+file_get_contents($website . "/sendmessage?chat_id=" . $chatId . "&text=" . $output);
